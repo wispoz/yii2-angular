5758d0ecf83ceaa63ceb31c3610c5e81f0323863
biziwalker <biziwalker@gmail.com>
2014-05-09 16:22:32 +0700

    Add Angular.js 1.2.16

---
+++ AngularAsset.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @link http://www.tesjin.ru/
+ * @copyright Copyright (c) 2014 TesJin Group
+ * @license BSD-3-Clause
+ */
+
+namespace yii\angularjs;
+
+/**
+ * This asset bundle provides the [angular javascript library](https://angularjs.org/)
+ *
+ * @author Vladislav Orlov <orlov@tesjin.ru>
+ */
+class AngularAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/tesjin/yii2-angularjs';
+    public $js = [
+        'js/angular.js',
+        'js/angular-animate.js',
+        'js/angular-cookies.js',
+        'js/angular-loader.js',
+        'js/angular-mocks.js',
+        'js/angular-resource.js',
+        'js/angular-route.js',
+        'js/angular-sanitize.js',
+        'js/angular-scenario.js',
+        'js/angular-touch.js',
+    ];
+}
