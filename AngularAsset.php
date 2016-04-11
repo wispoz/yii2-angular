15146ae456479d5e7e69d038603bbc5cc930232a
wispoz <wispoz@inbox.ru>
2016-04-11 17:11:24 +0300

    fix bower

--- AngularAsset.php
+++ AngularAsset.php
@@ -3,6 +3,7 @@ namespace wispoz\angularjs;
 use yii\web\AssetBundle;
 class AngularAsset extends AssetBundle
 {
+
     public $sourcePath = '@bower/angular';
     public $js = [
         'angular.js',
