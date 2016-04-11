950f14951120921470d5c0ba9e9057925b3ab494
wispoz <wispoz@inbox.ru>
2016-04-11 16:46:28 +0300

    remove oldies

--- AngularAsset.php
+++ AngularAsset.php
@@ -1,20 +1,6 @@
 <?php
-/**
- * @link http://www.tesjin.ru/
- * @copyright Copyright (c) 2014 TesJin Group
- * @license BSD-3-Clause
- */
-
 namespace wispoz\angularjs;
-
 use yii\web\AssetBundle;
-
-/**
- * This asset bundle provides the [angular javascript library](https://angularjs.org/)
- *
- * @author Vladislav Orlov <orlov@tesjin.ru>
- * @author wispoz <wispoz@inbox.ru>
- */
 class AngularAsset extends AssetBundle
 {
     public $sourcePath = 'bower-asset/angular';
